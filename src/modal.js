b727d3e6b5689cd54fa3671d42ee33f920645eda
Parminder Gill <pgill18@gmail.com>
2016-07-25 02:17:23 -0700

    edit

--- src/modal.js
+++ src/modal.js
@@ -2,16 +2,12 @@
 	var modal = document.getElementById('myModal');
 
 	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
 	var link = document.getElementById("modal1");
 
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
 
 	// When the user clicks the button, open the modal
-	btn.onclick = function() {
-	    modal.style.display = "block";
-	}
 	link.onclick = function() {
 	    modal.style.display = "block";
 	}
